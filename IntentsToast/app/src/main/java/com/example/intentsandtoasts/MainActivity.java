9e3311fd8d3b66cda3b249bf39975d1d4a98c99d
Ronaldyika <buhnyuyronald@gmail.com>
2024-04-03 13:55:41 +0100

    added some comments on the room db insert section

--- IntentsToast/app/src/main/java/com/example/intentsandtoasts/MainActivity.java
+++ IntentsToast/app/src/main/java/com/example/intentsandtoasts/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.intentsandtoasts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+//Romm db classes for insert delete update and list
+
+    public void goToActivityB(View v){
+        Intent intent = new Intent(MainActivity.this,ActivityB.class);
+        startActivity(intent);
+    }
+
+    public void performToast(View v){
+        Toast i = Toast.makeText(MainActivity.this,"a toast was started",Toast.LENGTH_LONG);
+        i.show();
     }
 }
